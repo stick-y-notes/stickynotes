ffc8dc8034af2e3363a90faaa711e33dfcd5afad
The SunShining <thesunshiningmin@gmail.com>
2025-04-30 20:16:12 -0500

    js

---
+++ src/js/app/note.js
@@ -0,0 +1,4 @@
+// notes 
+// create 
+// edit
+// list
---
+++ src/js/hello.js
@@ -0,0 +1,6 @@
+// 🟨 Sticky Note JS
+export const Sticky_Note_JS_message = "🟨 Welcome to Sticky Note! JavaScript is loaded.";
+
+
+// 🟨 Sticky Note JS
+console.log(Sticky_Note_JS_message);
