bb8255403af859f8a608cfdb78d597720dc16eb9
The SunShining <thesunshiningmin@gmail.com>
2025-04-30 21:44:58 -0500

    sw pwa

---
+++ src/sw.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'stickynotes-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.html',
+  './css/main.css',
+  './css/style.css',
+  './css/app.css',
+  './js/hello.js',
+  './js/app/note.js',
+  './js/app/note_options.js',
+  './img/icon.svg',
+  './manifest.json'
+];
+
+// Install service worker and cache all assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate service worker and clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
+
+// Periodic update check (every 24 hours)
+setInterval(() => {
+  if (navigator.onLine) {
+    caches.delete(CACHE_NAME).then(() => {
+      caches.open(CACHE_NAME).then(cache => {
+        cache.addAll(ASSETS_TO_CACHE);
+      });
+    });
+  }
+}, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
