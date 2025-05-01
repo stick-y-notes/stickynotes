0d24591b9ec77b0a71cd70564d693d181764abbf
The SunShining <thesunshiningmin@gmail.com>
2025-04-30 22:19:06 -0500

    Update hello.js

--- src/js/hello.js
+++ src/js/hello.js
@@ -4,22 +4,3 @@ console.log(Sticky_Note_JS_message);
 
 
 
-
-// ❌ 📲
-// Mobile device detection
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-// Add event listener to ENTER_BUTTON
-document.addEventListener('DOMContentLoaded', () => {
-    const enterButton = document.getElementById('ENTER_BUTTON');
-    if (enterButton) {
-        enterButton.addEventListener('click', (e) => {
-            if (isMobileDevice()) {
-                e.preventDefault();
-                alert('Sorry, this app is currently only available on desktop devices.');
-            }
-        });
-    }
-});
