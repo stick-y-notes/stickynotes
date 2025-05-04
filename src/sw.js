0f08dcfa960a46b479e9c2e1d02376e8ebb69633
The SunShining <thesunshiningmin@gmail.com>
2025-05-03 19:31:13 -0500

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stickynotes-v1';
+const CACHE_NAME = 'stickynotes-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -24,6 +24,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -38,6 +39,9 @@ self.addEventListener('activate', (event) => {
           }
         })
       );
+    }).then(() => {
+      // Check for old service workers and unregister them
+      return self.clients.claim();
     })
   );
 });
